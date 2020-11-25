ed9f1d3e69a6ea08411816b18784f5c721cdab9d
Sven Assmann <sven.assmann.it@gmail.com>
2020-11-25 20:24:48 +0100

    fix(tests): add a test case for the invalid case
     - make sure the 10.14 bug happens happening on homebrew build does not happend again
     - make sure that invalid captured images (buffer and metadata mismatch) yield the same error as if capture was not possible

--- src/macos/screenshot.rs
+++ src/macos/screenshot.rs
@@ -1,5 +1,5 @@
 use crate::ImageOnHeap;
-use anyhow::{Context, Result};
+use anyhow::{ensure, Context, Result};
 use core_graphics::display::*;
 use core_graphics::image::CGImageRef;
 use image::flat::SampleLayout;
@@ -23,18 +23,23 @@ pub fn capture_window_screenshot(win_id: u32) -> Result<ImageOnHeap> {
         ))?;
 
         let img_ref: &CGImageRef = &image;
+        // CAUTION: the width is not trust worthy, only the buffer dimensions are real
         let (_wrong_width, h) = (img_ref.width() as u32, img_ref.height() as u32);
         let raw_data: Vec<_> = img_ref.data().to_vec();
         let byte_per_row = img_ref.bytes_per_row() as u32;
+        // the buffer must be as long as the row length x height
+        ensure!(
+            byte_per_row * h == raw_data.len() as u32,
+            format!(
+                "Cannot grab screenshot from CGDisplay of window id {}",
+                win_id
+            )
+        );
         let bit_per_pixel = img_ref.bits_per_pixel() as u32;
         let channels = img_ref.bits_per_component() as u32 / 8;
-        // the buffer must be as long as the row length x height
-        assert_eq!(byte_per_row * h, raw_data.len() as u32);
-        // CAUTION this took me hours of my life time to figure out,
-        // the width is not trust worthy, only the buffer dimensions are real
-        // actual width, based on the buffer dimensions
-        let w = byte_per_row / ((bit_per_pixel / 8) * channels);
-        assert_eq!(bit_per_pixel / 8 * w * channels, byte_per_row);
+        let byte_per_pixel = bit_per_pixel / 8;
+        // the actual width based on the buffer dimensions
+        let w = byte_per_row / (byte_per_pixel * channels);
 
         (w, h, channels as u8, raw_data)
     };
@@ -47,4 +52,15 @@ pub fn capture_window_screenshot(win_id: u32) -> Result<ImageOnHeap> {
     };
 
     Ok(ImageOnHeap::new(buffer))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Cannot grab screenshot from CGDisplay of window id 999999")]
+    fn should_throw_on_invalid_window_id() {
+        capture_window_screenshot(9999999).unwrap();
+    }
 }
