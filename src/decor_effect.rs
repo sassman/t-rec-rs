7c259cb997bc4ad4688493d734cf5e5613a3b9f8
Sven Assmann <sven.assmann.it@gmail.com>
2020-11-30 00:30:11 +0100

    feat(decor:big-sur): add decor effect for big sur corners
     - on big sur the corner radius has increased and the captures screenshots had little white corners, this is now fixed and they are cut out
     - issue that sometimes there was a little pixel stripe on the right side, is now also fixed via trim'ing post processing magic

--- src/decor_effect.rs
+++ src/decor_effect.rs
@@ -1,7 +1,8 @@
 use crate::{file_name_for, Result};
 
 use anyhow::Context;
-use std::process::Command;
+use std::path::PathBuf;
+use std::process::{Child, Command};
 use tempfile::TempDir;
 
 ///
@@ -14,12 +15,11 @@ use tempfile::TempDir;
 ///     -layers merge \
 ///     t-rec-frame-000000251.tga
 /// ```
-///
-pub fn apply_shadow_decor_effect(time_codes: &[u128], tempdir: &TempDir) -> Result<()> {
-    let mut results = Vec::new();
-    for tc in time_codes.iter() {
-        let file = tempdir.path().join(file_name_for(tc, "tga"));
-        results.push(
+pub fn apply_shadow_effect(time_codes: &[u128], tempdir: &TempDir) -> Result<()> {
+    apply_effect(
+        time_codes,
+        tempdir,
+        Box::new(move |file| {
             Command::new("convert")
                 .arg(file.to_str().unwrap())
                 .arg("(")
@@ -29,8 +29,67 @@ pub fn apply_shadow_decor_effect(time_codes: &[u128], tempdir: &TempDir) -> Resu
                 .args(&["-layers", "merge"])
                 .arg(file.to_str().unwrap())
                 .spawn()
-                .context("Cannot apply decor effect")?,
-        );
+                .context("Cannot apply shadow decor effect")
+        }),
+    )
+}
+
+///
+/// apply a corner radius decor effect via a chain of convert commands
+/// this makes sure big sur corner radius, that comes in with white color, does not mess up
+///
+/// ```sh
+/// convert t-rec-frame-000000251.tga \
+///     -trim \( +clone  -alpha extract \
+///         -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 15,15 15,0' \
+///         \( +clone -flip \) -compose Multiply -composite \
+///         \( +clone -flop \) -compose Multiply -composite \
+///      \) -alpha off -compose CopyOpacity -composite \
+///    t-rec-frame-000000251.tga
+/// ```
+pub fn apply_big_sur_corner_effect(time_codes: &[u128], tempdir: &TempDir) -> Result<()> {
+    let radius = 13;
+    apply_effect(
+        time_codes,
+        tempdir,
+        Box::new(move |file| {
+            Command::new("convert")
+                .arg(file.to_str().unwrap())
+                .arg("-trim")
+                .arg("(")
+                .args(&["+clone", "-alpha", "extract"])
+                .args(&[
+                    "-draw",
+                    &format!(
+                        "fill black polygon 0,0 0,{r} {r},0 fill white circle {r},{r} {r},0",
+                        r = radius
+                    ),
+                ])
+                .args(&["(", "+clone", "-flip", ")"])
+                .args(&["-compose", "Multiply", "-composite"])
+                .args(&["(", "+clone", "-flop", ")"])
+                .args(&["-compose", "Multiply", "-composite"])
+                .arg(")")
+                .args(&["-alpha", "off", "-compose", "CopyOpacity", "-composite"])
+                .arg(file.to_str().unwrap())
+                .spawn()
+                .context("Cannot apply corner decor effect")
+        }),
+    )
+}
+
+///
+/// apply a given effect (closure) to all frames
+///
+fn apply_effect(
+    time_codes: &[u128],
+    tempdir: &TempDir,
+    effect: Box<dyn Fn(PathBuf) -> Result<Child>>,
+) -> Result<()> {
+    let mut results = Vec::new();
+    for tc in time_codes.iter() {
+        let file = tempdir.path().join(file_name_for(tc, "tga"));
+        results.push(effect(file.into())?);
     }
 
     for mut r in results {
--- src/macos/screenshot.rs
+++ src/macos/screenshot.rs
@@ -6,7 +6,7 @@ use image::flat::SampleLayout;
 use image::{ColorType, FlatSamples};
 
 pub fn capture_window_screenshot(win_id: u32) -> Result<ImageOnHeap> {
-    let (w, h, channels, raw_data, delta_width) = {
+    let (w, h, channels, raw_data) = {
         let image = unsafe {
             CGDisplay::screenshot(
                 CGRectNull,
@@ -24,7 +24,7 @@ pub fn capture_window_screenshot(win_id: u32) -> Result<ImageOnHeap> {
 
         let img_ref: &CGImageRef = &image;
         // CAUTION: the width is not trust worthy, only the buffer dimensions are real
-        let (wrong_width, h) = (img_ref.width() as u32, img_ref.height() as u32);
+        let (_wrong_width, h) = (img_ref.width() as u32, img_ref.height() as u32);
         let raw_data: Vec<_> = img_ref.data().to_vec();
         let byte_per_row = img_ref.bytes_per_row() as u32;
         // the buffer must be as long as the row length x height
@@ -39,13 +39,8 @@ pub fn capture_window_screenshot(win_id: u32) -> Result<ImageOnHeap> {
         let channels = img_ref.bits_per_component() as u32 / 8;
         // the actual width based on the buffer dimensions
         let w = byte_per_row / (byte_per_pixel * channels);
-        // dbg!(wrong_width, w);
-        let d = wrong_width as i64 - w as i64;
-        // if d != 0 {
-        //     eprintln!("width delta: {}", d);
-        // }
 
-        (w, h, channels as u8, raw_data, d)
+        (w, h, channels as u8, raw_data)
     };
 
     let color = ColorType::Bgra8;
