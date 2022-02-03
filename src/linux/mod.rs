0cb032e94174d009f310da0ea9db7e993ba28f1d
Sven Assmann <sven.assmann.it@gmail.com>
2022-02-03 23:08:18 +0100

    fix(clippy): make clippy happy

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -4,7 +4,7 @@ use crate::{PlatformApi, Result};
 use x11_api::X11Api;
 
 pub fn setup() -> Result<impl PlatformApi> {
-    Ok(X11Api::new()?)
+    X11Api::new()
 }
 
 pub const DEFAULT_SHELL: &str = "/bin/sh";
