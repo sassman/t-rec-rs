9182a9937b34358ebe9a9666cbceea0ae7253ab5
Sven Kanoldt <sven@d34dl0ck.me>
2025-12-14 22:55:14 +0100

    fix: fix outdated convert command call in docblock
    Signed-off-by: Sven Kanoldt <sven@d34dl0ck.me>

--- src/decors/big_sur_corner.rs
+++ src/decors/big_sur_corner.rs
@@ -12,7 +12,7 @@ use crate::Result;
 ///
 /// ```sh
 /// convert t-rec-frame-000000251.bmp \
-///     -trim \( +clone  -alpha extract \
+///     \( +clone  -alpha extract \
 ///         -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 15,15 15,0' \
 ///         \( +clone -flip \) -compose Multiply -composite \
 ///         \( +clone -flop \) -compose Multiply -composite \
